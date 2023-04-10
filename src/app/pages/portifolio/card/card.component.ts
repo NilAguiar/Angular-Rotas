06ded8d2fc956ad4437f61ba79e311e8af431903
NilAguiar <eleniltonvicentedeaguiar@outlook.com>
2023-04-10 09:29:48 -0300

    Trabalhando com rotas no Angular

---
+++ src/app/pages/portifolio/card/card.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private navegador: Router
+  ){
+
+    // http://localhost:4200/portifolio/{1}
+    this.activeRoute.params.subscribe(
+      res => console.log(res)
+    )
+
+    // Rotas filhas
+    this.activeRoute.firstChild?.params.subscribe(
+      res => console.log(res)
+    )
+
+    // http://localhost:4200/portifolio/1?{nome=nil&idade=36}
+    this.activeRoute.queryParams.subscribe(
+      res => console.log(res)
+    )
+  }
+  ngOnInit(): void {
+    // setInterval(() => {
+    //   this.navegador.navigate(['/'])
+    // }, 5000)
+  }
+}
